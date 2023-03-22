fd64132ef2056d947caa459e32d5c1bfb916093d
Avtomatplis <119958728+Avtomatplis@users.noreply.github.com>
2023-03-22 19:42:32 +0300

    Lav4_PDSD

---
+++ SetLav4_PDSD/SetLab4_Zhigalov.cpp
@@ -0,0 +1,192 @@
+#include "SetLab4_Zhigalov.h"
+#include <iostream>
+#include <random>
+#include <string>
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+
+using namespace std;
+
+Set::Set() //создание пустого множества
+{
+	this->ContainerListSet = list<int>();
+}
+
+
+bool Set::emptySetList() //Пустое ли множество? (F2)
+{
+	return this->ContainerListSet.empty();
+}
+
+bool Set::elemInSetList(int values) //принадлежность элемента множеству (F3)
+{
+	list<int>::const_iterator it = find(this->ContainerListSet.begin(), this->ContainerListSet.end(), values);
+	if (it != this->ContainerListSet.end()) {
+		return true;
+	}
+	return false;
+}
+
+void Set::addelem(int values) //добавление нового элемента в множество (F4)
+{
+	if (elemInSetList(values))
+	{
+		return;
+	}
+	else
+	{
+		this->ContainerListSet.push_front(values);
+
+	}
+}
+
+int Set::random(int min, int max)//генерация рандомных значений
+{
+	uniform_int_distribution<> dist(min, max);
+	return dist(gen);
+}
+/*
+   Вариант 7: Множество А - множество чисел, кратных 7. Множество B - множество нечетных чисел.
+*/
+Set::Set(int len, int min, int max, char setType) //создаём множество (F5)
+{
+	this->ContainerListSet = list<int>();
+
+	if (min > max || len <= 0 || max - min < len)
+	{
+		return;
+	}
+	list<int>::const_iterator pred_node = this->ContainerListSet.begin();
+	while (len > 0)
+	{
+		int elem = random(min, max);
+		if (setType == 'A' && elem % 7 == 0)//множество А из чисел кратных 7
+		{
+
+			addelem(elem);
+			if (this->ContainerListSet.begin() != pred_node)
+			{
+				len--;
+			}
+			pred_node = this->ContainerListSet.begin();
+
+		}
+		if (setType == 'B' && elem % 2 == 1)//Множество B из нечетных чисел
+		{
+
+			addelem(elem);
+			if (this->ContainerListSet.begin() != pred_node)
+			{
+				len--;
+			}
+			pred_node = this->ContainerListSet.begin();
+
+		}
+
+	}
+}
+
+
+
+int Set::Power_SetList() //вычисление мощности множества (F6)
+{
+	return this->ContainerListSet.size();
+}
+
+string Set::toString(char sym)//вывод значений в виде строки (F7)
+{
+	if (!emptySetList())
+	{
+		string result = "";
+
+		for (int elem : this->ContainerListSet)
+		{
+			result += to_string(elem);//преобразуем значение в строку
+			result += sym;//разделители
+		}
+		int len = result.length();
+		return result.erase(len - 1, len); //убираем последний разделитель
+	}
+	return "";
+}
+
+Set::~Set()//удаление множества (F8)
+{
+	this->ContainerListSet.clear();
+
+}
+
+//F9
+bool Set::isSubset(Set* sub_set)//является ли А подмножеством B
+{
+	if (emptySetList())
+	{
+		return true;
+	}
+	for (int sub_elem : sub_set->ContainerListSet)
+		if (!elemInSetList(sub_elem))
+		{
+			return false;
+		}
+	return true;
+}
+
+//F10
+bool Set::equalSet(Set* set_B)//равенство двух множеств А-В
+{
+	return isSubset(set_B) && set_B->isSubset(this);
+
+}
+
+//F11
+Set* Set::unitySet(Set* set_B)//объединение двух множеств
+{
+	Set* set_C = new Set();
+	for (int elem_A : this->ContainerListSet)
+	{
+		set_C->addelem(elem_A);
+
+	}
+	for (int elem_B : set_B->ContainerListSet)
+	{
+		set_C->addelem(elem_B);
+	}
+	return set_C;
+
+}
+
+//F12
+Set* Set::intersectionSet(Set* set_B)//Пересечение двух множеств
+{
+	Set* set_C = new Set();
+	for (int elem_B : set_B->ContainerListSet)
+		if (elemInSetList(elem_B))
+		{
+			set_C->addelem(elem_B);
+		}
+	return set_C;
+}
+
+//F13
+Set* Set::differenceSet(Set* set_B)//разность двух множеств(есть в А но нет в В)
+{
+	Set* set_C = new Set();
+	for (int elem_B : this->ContainerListSet)
+		if (!set_B->elemInSetList(elem_B))
+		{
+			set_C->addelem(elem_B);
+		}
+	return set_C;
+}
+
+//F14
+Set* Set::symmetrydifferentSet(Set* set_B)//Симметричная разность двух множеств
+//элементы только из 1 и только из 2 множества
+// т.е.2 множества без одинаковых
+{
+	Set* unitySets = unitySet(set_B);//объдиняем
+	Set* intersectionSets = intersectionSet(set_B);//ищем одинаковые(пересечение)
+	return unitySets->differenceSet(intersectionSets);//разность множеств
+}
---
+++ SetLav4_PDSD/Set_Cont_List_Zhigalov.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+
+#include "SetLab4_Zhigalov.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	Set* SetList_A = new Set(Set::random(6, 9), 10, 99, 'A');
+	Set* SetList_B = new Set(Set::random(6, 9), 10, 99, 'B');
+	cout << "Создание множества А: " << SetList_A->toString(' ') << endl;
+	cout << "Создание множества B: " << SetList_B->toString(' ') << endl;
+	cout << "Мощность множества А: " << SetList_A->Power_SetList() << endl;
+	cout << "Мощность множества B: " << SetList_B->Power_SetList() << endl;
+	cout << "Вывод множества А: " << SetList_A->toString(' ,') << endl;
+	cout << "Вывод множества А: " << SetList_B->toString(' ,') << endl;
+	cout << "" << endl;
+	cout << "Подмножество А-B: " << boolalpha << SetList_A->isSubset(SetList_B) << endl;
+	cout << "Подмножество А-A: " << boolalpha << SetList_A->isSubset(SetList_A) << endl;
+	cout << "Равенство двух множеств А-B: " << boolalpha << SetList_A->equalSet(SetList_B) << endl;
+	cout << "Равенство двух множеств А-A: " << boolalpha << SetList_A->equalSet(SetList_A) << endl;
+	cout << "Объединение двух множеств А-B: " << SetList_A->unitySet(SetList_B)->toString(' ') << endl;
+	cout << "Пересечение двух множеств А-B: " << SetList_A->intersectionSet(SetList_B)->toString(' ') << endl;
+	cout << "Разность двух множеств А-B: " << SetList_A->differenceSet(SetList_B)->toString(' ') << endl;
+	cout << "Разность двух множеств B-A: " << SetList_B->differenceSet(SetList_A)->toString(' ') << endl;
+	cout << "Cимметричная разность двух множеств А-В: " << SetList_A->symmetrydifferentSet(SetList_B)->toString(' ') << endl;
+	cout << "" << endl;
+	cout << "Удаление множества А " << endl;
+	delete SetList_A;
+	SetList_A = nullptr;
+	cout << "Удаление множества B " << endl;
+	delete SetList_B;
+	SetList_B = nullptr;
+}
